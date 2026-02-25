2d211fad86eacf77005de402146badb80c0a95bb
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2026-02-25 11:41:19 +0100

    feat: part 3 phonebook update dist

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl =
+    import.meta.env.VITE_ENVIRONMENT === "development" ? "http://localhost:3001/api/persons" : "/api/persons";
 
 const getAll = () => {
     const request = axios.get(baseUrl);
